73be16acfd91dd257df9a4b4ad3b0d0024aba5ed
marcobenegas <marcobenegas@ecomputer.es>
2022-11-17 09:58:53 +0100

    ME/02: implemented event clickable for home view.

--- app/src/main/java/com/example/mobile_etno/MainActivity.kt
+++ app/src/main/java/com/example/mobile_etno/MainActivity.kt
@@ -6,18 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mobile_etno.components.TopBarHome
-import com.example.mobile_etno.screens.ScreenHome
-import com.example.mobile_etno.ui.theme.MobileetnoTheme
 import com.example.mobile_etno.utils.colors.Colors
 import com.example.mobile_etno.viewmodels.MenuViewModel
+import com.example.mobile_etno.views.ScreenHome
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Box(modifier = Modifier
                 .fillMaxSize()
                 .background(Colors.backgroundEtno)) {
-                ScreenHome(viewModel = menuViewModel)
+                ScreenHome(viewModel = menuViewModel, applicationContext)
             }
         }
     }
