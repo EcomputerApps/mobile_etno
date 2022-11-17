fba25dd7c28963bcefdf9d49ee26e27fbc097647
marcobenegas <marcobenegas@ecomputer.es>
2022-11-17 09:44:56 +0100

    hotfix: fixed structure

--- app/src/main/java/com/example/mobile_etno/viewmodels/MenuViewModel.kt
+++ app/src/main/java/com/example/mobile_etno/viewmodels/MenuViewModel.kt
@@ -3,6 +3,7 @@ package com.example.mobile_etno.viewmodels
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.mobile_etno.R
 import com.example.mobile_etno.models.MenuItem
 import com.google.gson.Gson
 import kotlinx.coroutines.launch
@@ -10,14 +11,21 @@ import java.io.File
 
 class MenuViewModel: ViewModel() {
     private val gson = Gson()
-    var listMenu: List<MenuItem> = mutableStateListOf()
-
-    init {
-        viewModelScope.launch{
-            //val fileRead:JsonReader = JsonReader(FileReader("/menu_items.json"))
-            val file = File("/menu_items.json").inputStream().readBytes().toString()
-            val listFromJson: Array<MenuItem> = gson.fromJson(file, Array<MenuItem>::class.java)
-            listMenu = listFromJson.toList()
-        }
-    }
+    var listMenu: List<MenuItem> = mutableStateListOf(
+        MenuItem("Events"),
+        MenuItem("Reservations"),
+        MenuItem("Deaths"),
+        MenuItem("Phone"),
+        MenuItem("News"),
+        MenuItem("Gallery"),
+        MenuItem("Pharmacies"),
+        MenuItem("Sponsors"),
+        MenuItem("Festivities"),
+        MenuItem("Advertisements"),
+        MenuItem("Tourism"),
+        MenuItem("Services"),
+        MenuItem("Incidents"),
+        MenuItem("Links"),
+        MenuItem("Bandos")
+    )
 }
